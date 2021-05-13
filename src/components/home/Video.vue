7c6f55baa9099f12c1cdda09a36c1a26c41053b2
guillermopineda <guillermo.pineda@gn10.com>
2021-05-12 21:26:49 -0500

    se agrega video

--- src/components/home/Video.vue
+++ src/components/home/Video.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <video class="videoPeony" type="video" autoplay loop muted playsinline>
+    <video class="videoPeony" type="video" playsinline autoplay muted loop>
       <source src="@/assets/peony.mp4" type="video/mp4" /> 
     </video>
    
-    <video class="videoPeony916" type="video" autoplay loop muted  playsinline>
+    <video class="videoPeony916" type="video" playsinline autoplay muted loop>
       <source src="@/assets/peony9x16.mp4" type="video/mp4" />
     </video>
   </div>
