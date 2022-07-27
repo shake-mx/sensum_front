3c472cfbe9cb5429d3037c5832ea9755830fb31a
Guillermo Pineda <guillermopineda@Mac-mini-de-Guillermo.local>
2022-07-26 23:10:19 -0500

    fix javascript

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,19 @@ module.exports = {
   chainWebpack: config => {
     const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
     types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Sensum Soluciones Inmobiliarias'
+        return args
+      }, [])
   },
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: []
+    }
+  }
 
 }
 
